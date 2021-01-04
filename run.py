864367aa4bc56fc0e1087f3a9dec173620afd66e
Precillieo <kolawoleprecious99@gmail.com>
2021-01-04 03:14:55 +0100

    run

---
+++ run.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, url_for
+
+app= Flask(__name__)
+
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+	return render_template('home.html')
+	
+
+@app.route("/about")
+def about():
+	return render_template('about.html')
+
+@app.route("/portfolio", methods= ['GET', 'POST'])
+def portfolio():
+	return render_template('portfolio.html')
+
+
+@app.route("/contact")
+def contact():
+	return render_template('contact.html')
+
+
+if __name__=="__main__":
+	app.run(debug= True)
