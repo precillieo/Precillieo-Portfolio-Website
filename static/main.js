acb3a83eb991be596cb57b6a8667366cd9f44be1
Tobi Ajibade <herityjohnny14>
2021-01-11 22:09:25 -0800

    changed site layout structure

---
+++ static/main.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log("In dev mode");
+
+// This code automatically insert and update the current yeat in the footer area
+
+const currentYear = document.querySelector(".currentYear");
+const Year = new Date().getFullYear();
+currentYear.innerHTML = Year;
