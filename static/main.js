19b6312ac7fc344a0a402541472cdf91d0d24368
Tobi Ajibade <herityjohnny14>
2021-01-13 20:42:06 -0800

    Restructured the about page

--- static/main.js
+++ static/main.js
@@ -1,8 +1,15 @@
 "use strict";
-console.log("In dev mode");
+// console.log("In dev mode");
 
 // This code automatically insert and update the current yeat in the footer area
 
 const currentYear = document.querySelector(".currentYear");
 const Year = new Date().getFullYear();
 currentYear.innerHTML = Year;
+
+// code to toggle dropdown for mobile nav
+
+
+
+
+// code to display active tabs
