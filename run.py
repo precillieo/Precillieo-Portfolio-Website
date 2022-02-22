8739e9f35074e210bc6a24765c7cc060d1f3cf66
Zainab Lawal <zainablaw1012@gmail.com>
2022-02-22 17:50:50 +0300

    Linked all pages

--- run.py
+++ run.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, url_for
 app= Flask(__name__)
 
 
-
 @app.route("/")
 @app.route("/home")
 def home():
@@ -19,10 +18,25 @@ def portfolio():
 	return render_template('portfolio.html')
 
 
-@app.route("/contact")
-def contact():
-	return render_template('contact.html')
+@app.route("/blog")
+def blog():
+	return render_template('blog.html')
 
+@app.route("/community")
+def community():
+	return render_template('community.html')
+
+@app.route("/talks")
+def talks():
+	return render_template('talks.html')
+
+@app.route("/awards")
+def awards():
+	return render_template('awards.html')
+
+@app.route("/work")
+def work():
+	return render_template('work.html')
 
 if __name__=="__main__":
 	app.run(debug= True)
