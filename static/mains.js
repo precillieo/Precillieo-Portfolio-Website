6e244203fbad6e13603abfb6bb2d5d31420a61cb
Zainab Lawal <zainablaw1012@gmail.com>
2022-03-22 11:16:32 +0300

    Blog Page

--- static/mains.js
+++ static/mains.js
@@ -6,3 +6,9 @@ function closeNav() {
     document.getElementById("Nav").style.width = "0%";
 }
 
+const accordion = document.getElementsByClassName('pcontainer');
+    for (i=0; i<accordion.length; i++) {
+    accordion[i].addEventListener('click', function () {
+        this.classList.toggle('active')
+    })
+}
